2d4ae8012a14e4d74505da4850a1da08a9322ee4
A Flores <ctrl4ltdeleteme@gmail.com>
2011-06-11 23:47:40 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Listy::Application.config.secret_token = '128293ae8f209fbdaed6dc5a8ebf5cd547f37e012d059a05a539775fef5af6199d6414a99b525e83e8fbc81857fd94454a9a47136c8e5b2d24030b0214a1811b'
